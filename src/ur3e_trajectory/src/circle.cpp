38ed6b2b20a36967591b69d56fd1531cf1a9ce93
madelinebrode <madelinebrode@gmail.com>
2023-12-12 10:23:19 -0500

    Update circle.cpp

--- src/ur3e_trajectory/src/circle.cpp
+++ src/ur3e_trajectory/src/circle.cpp
@@ -24,6 +24,47 @@ int main(int argc, char **argv)
     moveit::planning_interface::MoveGroupInterface arm_move_group("manipulator");
 
     //Write your code for following the circle trajectory here.
-     
+    moveit::planning_interface::MoveGroupInterface::Plan joint_plan;
+    //horizontal    
+    std::map<std::string, double> joint_targets;
+    joint_targets["elbow_joint"] = 0.0;
+    joint_targets["shoulder_lift_joint"] = -0.0;
+    joint_targets["shoulder_pan_joint"] = 0.0;
+    joint_targets["wrist_1_joint"] = -1.57;
+    joint_targets["wrist_2_joint"] = -1.57;
+    joint_targets["wrist_3_joint"] = 0.0;
+
+    bool joint_plan_success;
+    joint_plan_success = ArmController::planToJointTargets(planning_options, arm_move_group, joint_plan, joint_targets);
+
+    if(joint_plan_success){
+        ROS_INFO("Moving to joint target");
+        arm_move_group.execute(joint_plan);
+    } 
+
+    joint_targets["shoulder_pan_joint"] += 4*1.57;
+
+    joint_plan_success = ArmController::planToJointTargets(planning_options, arm_move_group, joint_plan, joint_targets);
+
+    if(joint_plan_success){
+        ROS_INFO("Moving to joint target");
+        arm_move_group.execute(joint_plan);
+    }
+
+    
+    /*
+    for(int i = 0;i<9;i++){
+        joint_targets["shoulder_pan_joint"] += 0.628;
+
+        bool joint_plan_success;
+        joint_plan_success = ArmController::planToJointTargets(planning_options, arm_move_group, joint_plan, joint_targets);
+
+        if(joint_plan_success){
+        ROS_INFO("Moving to joint target");
+        arm_move_group.execute(joint_plan);
+    } 
+    }
+
+    */
     
 }
